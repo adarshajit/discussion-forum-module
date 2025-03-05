28bd04e254b118d551550441c4d952a727734c0e
adarshajit <adarshajit@gmail.com>
2025-03-05 14:56:39 +0000

    chore: update allowed hosts for deployment

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -29,8 +29,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['https://discussion-forum-module.onrender.com/', 'localhost', '127.0.0.1']
-
+ALLOWED_HOSTS = ['discussion-forum-module.onrender.com', 'localhost', '127.0.0.1']
 
 # Application definition
 
