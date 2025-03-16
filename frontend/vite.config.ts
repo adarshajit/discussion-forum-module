85557c3074acbddfe5e7dea6b63b0fea693d3525
adarshajit <adarshajit@gmail.com>
2025-03-16 11:20:57 +0000

    fix: change base in vite config

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,5 +5,5 @@ import tailwindcss from '@tailwindcss/vite';
 // https://vite.dev/config/
 export default defineConfig({
 	plugins: [react(), tailwindcss()],
-	base: '/discussion-forum',
+	base: '/discussion-forum-module',
 });
