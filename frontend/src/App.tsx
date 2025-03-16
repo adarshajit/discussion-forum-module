65169fbee1b8ca98e8c529edde2f9ecd376ec27a
adarshajit <adarshajit@gmail.com>
2025-03-16 13:05:24 +0000

    fix: github pages refresh problem fixed

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,4 @@
-import { Routes, Route } from 'react-router';
+import { Routes, Route, BrowserRouter as Router} from 'react-router';
 import { AuthProvider } from './contexts/FakeAuthContext';
 import Login from './pages/Login';
 import Home from './pages/Home';
@@ -12,16 +12,18 @@ function App() {
 	return (
 		<>
 			<AuthProvider>
-				<Routes>
-					<Route path='/login' element={<Login />} />
-					<Route element={<AppLayout />}>
-						<Route path='/' element={<Home />} />
-						<Route path='/thread/create' element={<DiscussionCreation />} />
-						<Route path='/thread/:id' element={<DiscussionDetails />} />
-						<Route path='/profile' element={<Profile />} />
-						<Route path='*' element={<PageNotfound />} />
-					</Route>
-				</Routes>
+				<Router basename='/discussion-forum-module'>
+					<Routes>
+						<Route path='/login' element={<Login />} />
+						<Route element={<AppLayout />}>
+							<Route path='/' element={<Home />} />
+							<Route path='/thread/create' element={<DiscussionCreation />} />
+							<Route path='/thread/:id' element={<DiscussionDetails />} />
+							<Route path='/profile' element={<Profile />} />
+							<Route path='*' element={<PageNotfound />} />
+						</Route>
+					</Routes>
+				</Router>
 			</AuthProvider>
 		</>
 	);
