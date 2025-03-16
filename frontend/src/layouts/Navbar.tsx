cc0a74620a0b34677e3b3ce9027ca31d46fab259
adarshajit <adarshajit@gmail.com>
2025-03-16 12:16:10 +0000

    fix: minor changes to navbar and footer title

--- frontend/src/layouts/Navbar.tsx
+++ frontend/src/layouts/Navbar.tsx
@@ -36,7 +36,7 @@ const Navbar = () => {
 			</div>
 			<div className='navbar-center'>
 				<Link to='/' className='btn btn-ghost text-xl'>
-					URMS Discussions
+					Research Discussion Forum
 				</Link>
 			</div>
 			<div className='navbar-end gap-2'>
