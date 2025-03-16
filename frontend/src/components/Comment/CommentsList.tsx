aaff18d104d3af210d87d1a5d11d5eafaba8b900
adarshajit <adarshajit@gmail.com>
2025-03-16 11:16:20 +0000

    fix: remove unused variables

--- frontend/src/components/Comment/CommentsList.tsx
+++ frontend/src/components/Comment/CommentsList.tsx
@@ -7,7 +7,6 @@ import Loader from '../Loader';
 const CommentsList = ({ threadId }: { threadId: string | undefined }) => {
 	const [comments, setComments] = useState<Comment[]>([]);
 	const [loading, setLoading] = useState<boolean>(true);
-	const [error, setError] = useState<string | null>(null);
 
 	useEffect(() => {
 		const fetchThreadComments = async () => {
@@ -15,9 +14,7 @@ const CommentsList = ({ threadId }: { threadId: string | undefined }) => {
 				const data = await commentApi.getComments(threadId);
 				setComments(data.comments);
 			} catch (err) {
-				setError(
-					err instanceof Error ? err.message : 'Failed to fetch thread comments'
-				);
+				throw new Error("Failed to fetch thread comments!" + err)
 			} finally {
 				setLoading(false);
 			}
