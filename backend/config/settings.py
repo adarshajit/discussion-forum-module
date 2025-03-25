c9950c845b951974e049f721bf299c2452f87e27
adarshajit <adarshajit@gmail.com>
2025-03-25 21:08:45 +0000

    chore: remove unused cors link

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -150,7 +150,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
     "https://adarshajit.github.io",
-    "https://discussion-forum-module.vercel.app"
 ]
 
 
