b0ec5459dd8ab24b158bc6de4dc11b1273107603
adarshajit <adarshajit@gmail.com>
2025-03-25 21:07:47 +0000

    chore: install and setup react query

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,5 @@
-import { Routes, Route, BrowserRouter as Router} from 'react-router';
+import { Routes, Route, BrowserRouter as Router } from 'react-router';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { AuthProvider } from './contexts/FakeAuthContext';
 import Login from './pages/Login';
 import Home from './pages/Home';
@@ -8,23 +9,27 @@ import DiscussionDetails from './pages/DiscussionDetails';
 import PageNotfound from './pages/PageNotFound';
 import Profile from './pages/Profile';
 
+const queryClient = new QueryClient();
+
 function App() {
 	return (
 		<>
-			<AuthProvider>
-				<Router basename='/discussion-forum-module'>
-					<Routes>
-						<Route path='/login' element={<Login />} />
-						<Route element={<AppLayout />}>
-							<Route path='/' element={<Home />} />
-							<Route path='/thread/create' element={<DiscussionCreation />} />
-							<Route path='/thread/:id' element={<DiscussionDetails />} />
-							<Route path='/profile' element={<Profile />} />
-							<Route path='*' element={<PageNotfound />} />
-						</Route>
-					</Routes>
-				</Router>
-			</AuthProvider>
+			<QueryClientProvider client={queryClient}>
+				<AuthProvider>
+					<Router basename='/discussion-forum-module'>
+						<Routes>
+							<Route path='/login' element={<Login />} />
+							<Route element={<AppLayout />}>
+								<Route path='/' element={<Home />} />
+								<Route path='/thread/create' element={<DiscussionCreation />} />
+								<Route path='/thread/:id' element={<DiscussionDetails />} />
+								<Route path='/profile' element={<Profile />} />
+								<Route path='*' element={<PageNotfound />} />
+							</Route>
+						</Routes>
+					</Router>
+				</AuthProvider>
+			</QueryClientProvider>
 		</>
 	);
 }
